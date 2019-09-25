05225f8f2c0cd38aa0fb4e5427ac96681bf7898e
mansigoel98 <goel.mansi98@gmail.com>
2019-09-26 00:32:01 +0530

    data

--- app/src/main/java/com/example/pro1/Main2Activity.java
+++ app/src/main/java/com/example/pro1/Main2Activity.java
@@ -3,12 +3,16 @@ package com.example.pro1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
+    Button login,signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        login=findViewById(R.id.Login);
+        signUp=findViewById(R.id.SignUp);
     }
 }
--- app/src/main/java/com/example/pro1/MyPostCustomRequest.java
+++ app/src/main/java/com/example/pro1/MyPostCustomRequest.java
@@ -1,4 +1,69 @@
 package com.example.pro1;
 
-public class MyPostCustomRequest {
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.UnsupportedEncodingException;
+
+public class MyPostCustomRequest extends StringRequest {
+    private Response.Listener listener;
+
+    private Gson gson;
+
+    private TypeToken type;
+
+    private Class responseClass;
+
+    private String body;
+
+    public MyPostCustomRequest(int method, String url, Class responseClass, TypeToken type, String body, Response.Listener listener, Response.ErrorListener errorListener) {
+        super(method, url,listener ,errorListener);
+        gson = new Gson();
+        this.listener = listener;
+        this.responseClass = responseClass;
+        this.type = type;
+        this.body=body;
+    }
+
+    @Override
+    protected Response parseNetworkResponse(NetworkResponse response) {
+        try {
+
+            String jsonString = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+            if(type==null)
+                return Response.success(gson.fromJson(jsonString, responseClass), HttpHeaderParser.parseCacheHeaders(response));
+            else
+                return Response.success(gson.fromJson(jsonString, type.getType()), HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return Response.error(new ParseError(e));
+        }
+    }
+
+    @Override
+    public String getBodyContentType() {
+        return "application/json; charset=utf-8";
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError {
+        try {
+            return body == null ? null : body.getBytes("utf-8");
+        } catch (UnsupportedEncodingException uee) {
+
+            return null;
+        }
+    }
+
 }
